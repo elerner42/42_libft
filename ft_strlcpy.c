08e68e30a468df229624557baa5d743f199673b3
elerner <leo_kazo@hotmail.com>
2022-10-11 23:34:43 +0200

    ft_strlcpy v0

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 23:30:50 by elerner           #+#    #+#             */
+/*   Updated: 2022/10/11 23:30:50 by elerner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_s)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst_s > 0)
+	{
+		while (src[i] && i < (dst_s - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
