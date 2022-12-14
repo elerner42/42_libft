573cd6bb2d1c631ea44aeb77286313e334cb1165
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2022-12-14 17:52:53 +0000

    Libft_Update_Codespaces

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,14 +6,32 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 21:06:39 by elerner           #+#    #+#             */
-/*   Updated: 2022/12/13 17:01:24 by elerner          ###   ########.fr       */
+/*   Updated: 2022/12/14 17:34:17 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    //code
-    return (0);    
+	size_t	i;
+	size_t	dstlen;
+	size_t	j;
+
+	dstlen = ft_strlen(dst);
+	i = 0;
+	j = dstlen;
+	if (dstlen >= dstsize)
+		return (dstsize + ft_strlen(src));
+	if (dstsize > 0)
+	{
+		while (src[i] && dstlen + i < dstsize - 1)
+		{
+			dst[j] = src[i];
+			i++;
+			j++;
+		}
+		dst[j] = '\0';
+	}
+	return (dstlen + ft_strlen(src));
 }
