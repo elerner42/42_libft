ab25fdc7193e556c6e176b8475e585b5045a8407
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2022-12-19 17:47:40 +0000

    Libft_Upload

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/15 17:12:33 by elerner           #+#    #+#             */
-/*   Updated: 2022/12/19 16:55:42 by elerner          ###   ########.fr       */
+/*   Created: 2022/12/19 17:43:49 by elerner           #+#    #+#             */
+/*   Updated: 2022/12/19 17:46:45 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-	ft_calloc asigna  memoria  para  una  matriz  de  nmemb elementos 
-	de size bytes cada uno y devuelve un puntero a la memoria asignada. 
-	La memoria es puesta a cero.
-*/
-
-void	*ft_calloc(size_t c, size_t s)
+char *strdup(const char *str)
 {
-	void	*call;
-
-	call = (void *)malloc(c * s);
-	if (!call)
-		return (NULL);
-	ft_bzero(call, c * s);
-	return (call);
+    const int   len = ft_strlen(*str);
+    return (str);
 }
