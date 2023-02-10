7c317695dbadec15bbe5b962d7bfe5ad603f4bfc
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2023-02-10 22:55:01 +0000

    Update Libft - ft_strtrim terminada

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 20:07:29 by elerner           #+#    #+#             */
-/*   Updated: 2023/02/07 20:33:45 by elerner          ###   ########.fr       */
+/*   Updated: 2023/02/09 16:19:27 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,133 +24,25 @@
     malloc(3).
 */
 
-size_t	ft_strlen(const char *str)
+char	*ft_strtrim(char const *s, char const *set)
 {
-	size_t	l;
+	char	*s_tmp;
+	size_t	c;
+	size_t	len;
 
-	l = 0;
-	while (str[l])
+	s_tmp = NULL;
+	if (s && set)
 	{
-		l++;
-	}
-	return (l);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	unsigned char	ch;
-
-	ch = c;
-	while (*s)
-	{
-		if (*s == ch)
-			return ((char *)s);
-		if (*s == '\0')
-			return ((char *)s);
-		s++;
-	}
-	if (ch == '\0')
-		return ((char *)s);
-	return (NULL);
-}
-
-char	*ft_strrchr(const char *s, int c)
-{
-	char	*tmp;
-	char	ch;
-
-	ch = c;
-	tmp = NULL;
-	while (*s)
-	{
-		if (*s == ch)
-			tmp = (char *)s;
-		if (*s == '\0')
-			return (tmp);
-		s++;
-	}
-	if (ch == '\0')
-		return ((char *)s);
-	return (tmp);
-}
-
-void	ft_bzero(char *s, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)s)[i] = '\0';
-		i++;
-	}
-}
-
-void	*ft_calloc(size_t n_Element, size_t size_Element)
-{
-	char	*m_tmp;
-
-	if (size_Element != 0)
-		if (n_Element > __SIZE_MAX__ / size_Element)
+		len = ft_strlen(s);
+		c = 0;
+		while (s[c] && ft_strchr(set, s[c]))
+			c++;
+		while (s[len - 1] && ft_strrchr(set, s[len - 1]) && len > c)
+			len--;
+		s_tmp = ft_calloc((len - c) + 1, sizeof(char));
+		if (!s_tmp)
 			return (NULL);
-	m_tmp = malloc(n_Element * size_Element);
-	if (!m_tmp)
-		return (NULL);
-	ft_bzero(m_tmp, n_Element * size_Element);
-	return (m_tmp);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-
-	i = 0;
-	if (dstsize > 0)
-	{
-		while (src[i] && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = 0;
+		ft_strlcpy(s_tmp, &s[c], (len - c) + 1);
 	}
-	while (src[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*memory;
-	size_t	i;
-	size_t	size;
-
-	memory = NULL;
-	if (s1 && set)
-	{
-		i = 0;
-		size = ft_strlen(s1);
-		while (s1[i] && ft_strchr(set, s1[i]))
-			i++;
-		while (s1[size -1] && ft_strrchr(set, s1[size -1]) && size > i)
-			size--;
-		memory = (char *)ft_calloc((size - i + 1), sizeof(char));
-		if (memory == NULL)
-			return (NULL);
-		ft_strlcpy(memory, &s1[i], size - i + 1);
-	}
-	return (memory);
-}
-
-int main(void)
-{
-    char *Sa;
-    char *Sb;
-
-    Sa = "hola mundo";
-    Sb = "ohld";
-
-    printf("%s\n", ft_strtrim(Sa,Sb));
-    
-    return (0);
+	return (s_tmp);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 21:34:37 by elerner           #+#    #+#             */
-/*   Updated: 2023/02/07 20:07:49 by elerner          ###   ########.fr       */
+/*   Updated: 2023/02/09 18:45:05 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,8 @@ void		*ft_calloc(size_t n_Element, size_t size_Element);
 char		*ft_strdup(const char *str);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
-char 		*ft_strtrim(char const *s1, char const *set);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
 
 typedef struct s_list
 {
