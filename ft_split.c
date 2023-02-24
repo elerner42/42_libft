bab6f0a2389f1a6cb1856d1242b0f7b6cc3eb03a
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2023-02-24 03:21:55 +0000

    Update Libft - ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 16:28:37 by elerner           #+#    #+#             */
-/*   Updated: 2023/02/22 23:16:01 by elerner          ###   ########.fr       */
+/*   Updated: 2023/02/24 03:20:07 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,57 +25,98 @@
     array debe terminar con un puntero NULL.
 */
 
-/*char	**ft_split(char const *s, char c)
+/********** MI SPLIT *****************************************/
+size_t	ft_splitlen(const char *str, char c)
 {
-	char const	*s_tmp;
+	size_t	len;
+	size_t	pos;	
 
-	s_tmp = "Result";
-	return (*s_tmp);
-}
-
-https://parzibyte.me/blog/2018/11/13/separar-cadena-delimitadores-c-strtok/
-https://www.lawebdelprogramador.com/foros/C-Visual-C/1684922-Como-separar-cadenas-de-una-cadena-en-C.html
-https://www.youtube.com/watch?v=z_pukhnwjZc
-*/
-/*char	*ft_strchr(const char *s, int c)
-{
-	unsigned char	ch;
-
-	ch = c;
-	while (*s)
+	len = 0;
+	pos = 0;
+	if (str[pos] == c)
+		len--;
+	while(str[pos] != '\0')
 	{
-		if (*s == ch)
-			return ((char *)s);
-		if (*s == '\0')
-			return ((char *)s);
-		s++;
+		if (str[pos + 1] == '\0' || (str[pos] == c && str[pos + 1] != c))
+			len++;
+		pos++;
 	}
-	if (ch == '\0')
-		return ((char *)s);
-	return (NULL);
+	return (len);
+}
+/*size_t	ft_splitlen(const char *str, char c)
+{
+	size_t	len;
+	char	*tmp;
+	char	car[2];
+
+	car[0] = c;
+	car[1] = '\0';
+	tmp = ft_strtrim(str, car);
+	len = 0;
+	while (*tmp != '\0')
+	{
+		if (*tmp == car[0])
+			len++;
+		tmp++;
+	}
+	return (len);
 }*/
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	unsigned char	*str;
-	unsigned char	ch;
 
-	ch = (char)c;
-	str = (unsigned char *)s;
-	while (n > 0)
+char	*ft_settoken(const char *str, char c)
+{
+	size_t	len;
+	char	*tmp;
+
+	len = 0;
+	while (str[len] && str[len] != c)
+		len++;
+	tmp = (char *)malloc((len + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	len = 0;
+	while (str[len] && str[len] != c)
 	{
-		if (*str == ch)
-		{
-			return (str);
-		}
-		str++;
-		n--;
+		tmp[len] = str[len];
+		len++;
 	}
-	return (NULL);
+	tmp[len] = '\0';
+	return (tmp);
 }
 
+char	**ft_split(const char *str, char c)
+{
+	char	**table;
+	size_t	len;
+
+	len = 0;
+	if (!str)
+		return (NULL);
+	table = (char **)malloc((ft_splitlen(str, c) + 1) * sizeof(char *));
+	if (!table)
+		return (NULL);
+	while (*str)
+	{
+		while (*str == c && *str)
+			str++;
+		if (*str != c && *str)
+		{
+			table[len] = ft_settoken(str, c);
+			len++;
+			while (*str != c && *str)
+				str++;
+		}
+	}
+	table[len] = NULL;
+	return (table);
+}
+
+/*
 int main(){
-    /*char cadena[] = "Esto es un texto. Puede ir separado por puntos, espacios o comas.";
+    char cadena[] = "ooo,,,,,Est1o es un text2o, Puede ir, separad3o, p4or punt5os, espaci6os, c7omas.,8ooo";
+	//char cadena[100];
     char delimitador[] = "o";
+	
+	printf("Nro de Tokens: %li\n", ft_splitlen(cadena, 'o'));
     char *token = strtok(cadena, delimitador);
     if(token != NULL){
         while(token != NULL){
@@ -83,15 +124,40 @@ int main(){
             printf("Token: %s\n", token);
             token = strtok(NULL, delimitador);
         }
-    }*/
+
+	printf("-----------------------------------------\n");
+
+	char	**tab;
+	unsigned int	i;
+	
+	i = 0;
+	printf("Nro de Tokens: %i\n",str_count("ooo,,,,,Est1o es un text2o, Puede ir, separad3o, p4or punt5os, espaci6os, c7omas.,8ooo", 'o'));
+	tab = ft_split("ooo,,,,,Est1o es un text2o, Puede ir, separad3o, p4or punt5os, espaci6os, c7omas.,8ooo", 'o');
+	if (!tab[0])
+		puts("ok\n");
+	while (tab[i] != NULL)
+	{
+		printf("Token: %s\n", tab[i]);
+		i++;
+	}
+	//printf("\n%s\n", ft_strtrim("ooo,,,,,Est1o es un text2o, Puede ir, separad3o, p4or punt5os, espaci6os, c7omas.,ooo", "o"));
+	//puts("\n");
+	//printf("PRIMER:%s\n",ft_settoken(",,,,,Est1o es un text2o, Puede ir, separad3o, p4or punt5os, espaci6os, c7omas.,", 'o'));
+    }
+	/*
     int len;
     const char str[] = "Esto es un texto. Puede ir separado por puntos, espacios o comas.";
-    const char ch = 'k';
+    const char ch = 'e';
     char *ret;
 
-    ret = ft_memchr(str, ch, strlen(str)-1);
+	printf("Nro de Separadores: %li\n", ft_splitlen(str, ch));
+    ret = ft_memchr(str, ch, strlen(str)+1);
+	if(ret == NULL)
+	{ret = "No existe separador";}
 
     printf("%s\n",ret);
-   
-    return(0);
+
+	*/
+    /*return(0);
 }
+*/
