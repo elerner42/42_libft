9726e3485b78eafb30df406982861e9427aa90f3
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2023-03-08 22:03:19 +0000

    Libft_Update->Create more files

--- Makefile
+++ Makefile
@@ -35,7 +35,14 @@ SRCS			=	ft_bzero.c \
 					ft_substr.c \
 					ft_strjoin.c \
 					ft_strtrim.c \
-					ft_split.c
+					ft_split.c \
+					ft_itoa.c \
+					ft_strmapi.c \
+					ft_striteri.c \
+					ft_putchar_fd.c \
+					ft_putstr_fd.c \
+					ft_putendl_fd \
+					ft_putnbr_fd
 					
 OBJS			:= $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 21:34:37 by elerner           #+#    #+#             */
-/*   Updated: 2023/02/09 18:45:05 by elerner          ###   ########.fr       */
+/*   Updated: 2023/03/08 21:39:01 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,13 @@ char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
 typedef struct s_list
 {
