bd423c6758f8dca51b13baddacab3b9b56fae0e4
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2023-03-10 00:53:44 +0000

    libft Update - ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,63 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 21:16:02 by elerner           #+#    #+#             */
-/*   Updated: 2023/03/08 21:35:55 by elerner          ###   ########.fr       */
+/*   Updated: 2023/03/10 00:48:43 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+/*
+	la funcion recibe como parametro un dato de tipo "int", en otras palabras
+	puede ser un numero positivo y/o negativo.
+	Retorna un valor de tipo "char *", osea una cadena de caracteres.
+	gestionando los numeros negativos.
+*/
+
+static int	ft_sign(int n)
 {
-    return (0);
+	if (n < 0)
+		return (-1);
+	return (1);
+}
+
+static int	ft_lendigi(int n)
+{
+	int	d;
+
+	if (n == 0)
+		return (1);
+	d = 0;
+	while (n != 0)
+	{
+		d++;
+		n /= 10;
+	}
+	return (d);
+}
+
+char	*ft_itoa(int n)
+{
+	int		sign;
+	int		length;
+	int		tmp;
+	char	*str;
+
+	tmp = n;
+	length = ft_lendigi(n);
+	sign = ft_sign(n);
+	if (sign == -1)
+		length++;
+	str = malloc (sizeof(char) * (length + 1));
+	if (str == NULL)
+		return (NULL);
+	str[length] = '\0';
+	while (length--)
+	{
+		str[length] = ((n % 10) * sign) + '0';
+		n = n / 10;
+	}
+	if (sign == -1)
+		str[0] = '-';
+	return (str);
 }
