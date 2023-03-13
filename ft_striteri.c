2415849ab55e2e8a09e6a3c02272b8dc1386d4f7
Ernesto Lerner Viscarra <76266319+elerner42@users.noreply.github.com>
2023-03-13 23:58:49 +0000

    Libft Update -> Add comment!

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,11 +6,23 @@
 /*   By: elerner <elerner@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 21:42:06 by elerner           #+#    #+#             */
-/*   Updated: 2023/03/13 22:53:59 by elerner          ###   ########.fr       */
+/*   Updated: 2023/03/13 23:55:54 by elerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+	El prototipo de la función ft_striteri indica que esta función
+	toma una cadena de caracteres (char *s) y una función 
+	(void (*f)(unsigned int, char*)) como parámetros,
+	y no devuelve ningún valor (void).
+
+	La función ft_striteri aplica la función f a cada carácter de la cadena s,
+	pasando como parámetros el índice del carácter y una referencia 
+	al carácter en sí mismo. El índice se representa como un número 
+	entero sin signo (unsigned int).
+*/
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
